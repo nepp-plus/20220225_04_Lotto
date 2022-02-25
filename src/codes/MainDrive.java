4a439f880ec23e9d5498d6d6728c46edbc5f7533
nepp.plus <nepp.plus@gmail.com>
2022-02-25 15:07:16 +0900

    보너스번호 확인 / 주석 추가

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -140,7 +140,13 @@ public class MainDrive {
 		for (int num : winNumbers) {
 			System.out.println(num);
 		}
+		
+//		보너스번호도 확인
+		
+		System.out.println("보너스번호 : " + bonusNum);
 
+		
+//		맞춘 갯수 확인
 		
 		int correctCount = 0;
 
@@ -157,12 +163,16 @@ public class MainDrive {
 		}
 		
 		
+//		등수 판정
+		
 		if (correctCount == 6) {
 			System.out.println("1등");
 		}
 		else if (correctCount == 5) {
-//			보너스번호 로직 추가 필요
-			System.out.println("임시 - 3등");
+//			보너스번호 로직
+			
+			
+			
 		}
 		else if (correctCount == 4) {
 			
